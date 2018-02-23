09a353ca8e283a09a5b0b80991be3858ec11bf01
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-02-23 11:18:42 -0500

    A3Q2 in progress

---
+++ src/Assignment3/A3Q2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment3;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+
+/**
+ *
+ * @author salod8556
+ */
+public class A3Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //create city
+        City kw = new City();
+        
+        // create robot
+        RobotSE rob= new RobotSE(kw, 1, 1, Direction.EAST);
+        
+        //create things
+        new Thing(kw, 1, 1);
+        new Thing(kw, 1, 1);
+        new Thing(kw, 1, 1);
+        new Thing(kw, 1, 1);
+        new Thing(kw, 1, 1);
+        new Thing(kw, 1, 1);
+        new Thing(kw, 1, 1);
+        new Thing(kw, 1, 1);
+        new Thing(kw, 1, 1);
+        new Thing(kw, 1, 1);
+    }
+}
