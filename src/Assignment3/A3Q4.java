07969fa762133a10838a4f52e1fd758205fa26dc
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-02-27 11:08:49 -0500

    A3Q4 Finished

---
+++ src/Assignment3/A3Q4.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment3;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author salod8556
+ */
+public class A3Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //create city
+        City kw= new City();
+                
+       //create robot
+        RobotSE rob = new RobotSE(kw, 3, 3, Direction.SOUTH);
+        
+        //create walls
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 2, 1, Direction.WEST);
+        new Wall(kw, 2, 1, Direction.SOUTH);
+        new Wall(kw, 2, 2, Direction.SOUTH);
+        new Wall(kw, 2, 2, Direction.EAST);
+        new Wall(kw, 1, 2, Direction.EAST);
+        new Wall(kw, 1, 1, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.NORTH);
+        
+        new Wall(kw, 1, 4, Direction.WEST);
+        new Wall(kw, 2, 4, Direction.WEST);
+        new Wall(kw, 2, 4, Direction.SOUTH);
+        new Wall(kw, 2, 5, Direction.SOUTH);
+        new Wall(kw, 2, 5, Direction.EAST);
+        new Wall(kw, 1, 5, Direction.EAST);
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 5, Direction.NORTH);
+        
+        new Wall(kw, 4, 1, Direction.WEST);
+        new Wall(kw, 5, 1, Direction.WEST);
+        new Wall(kw, 5, 1, Direction.SOUTH);
+        new Wall(kw, 5, 2, Direction.SOUTH);
+        new Wall(kw, 5, 2, Direction.EAST);
+        new Wall(kw, 4, 2, Direction.EAST);
+        new Wall(kw, 4, 2, Direction.NORTH);
+        new Wall(kw, 4, 1, Direction.NORTH);
+        
+        new Wall(kw, 4, 4, Direction.WEST);
+        new Wall(kw, 5, 4, Direction.WEST);
+        new Wall(kw, 5, 5, Direction.SOUTH);
+        new Wall(kw, 5, 4, Direction.SOUTH);
+        new Wall(kw, 5, 5, Direction.EAST);
+        new Wall(kw, 4, 5, Direction.EAST);
+        new Wall(kw, 4, 4, Direction.NORTH);
+        new Wall(kw, 4, 5, Direction.NORTH);
+        
+        
+        // make robot so circles around boxes
+        for (int numberOfMoves =4; numberOfMoves >0; numberOfMoves--) {
+            for(int loop = 3; loop >0; loop--) {
+            rob.move(3);
+            rob.turnLeft();
+            
+          
+    }  
+            rob.move(3);
+}
+}
+}
