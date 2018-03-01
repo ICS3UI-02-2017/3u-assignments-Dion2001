ea6ce07b72e3e3556aa1c4f67613bfa27df8515f
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-03-01 11:15:20 -0500

    A4Q3 Finished

--- src/Assignment4/A4Q3.java
+++ src/Assignment4/A4Q3.java
@@ -4,6 +4,8 @@
  */
 package Assignment4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author salod8556
@@ -14,6 +16,21 @@ public class A4Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //create scanner
+        Scanner input= new Scanner( System.in);
+        
+        //ask for numbers
+        System.out.println("Please enter 4 numbers on different lines");
+        
+        //ask for 4 numbers
+        
+        String one =input.nextLine();
+        String two =input.nextLine();
+        String three =input.nextLine();
+        String four =input.nextLine();
+        
+        System.out.println("Your numbers were " + one + "," + two + "," + three + "," + four);
+        
+        
     }
 }
