4bff9f7216bc6302b6d64cb689b103e4c45a5158
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-03-02 11:15:04 -0500

    A4Q5 in Progress

---
+++ src/Assignment4/A4Q5.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author salod8556
+ */
+public class A4Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //create scanner
+       Scanner input= new Scanner(System.in);
+       
+       
+       
+       
+    }
+}
