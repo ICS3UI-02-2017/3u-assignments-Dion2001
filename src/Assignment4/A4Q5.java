a48879ab071ed33ae3d738d35085f924beb36390
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-03-05 11:18:49 -0500

    A4Q5

--- src/Assignment4/A4Q5.java
+++ src/Assignment4/A4Q5.java
@@ -18,6 +18,33 @@ public class A4Q5 {
     public static void main(String[] args) {
        //create scanner
        Scanner input= new Scanner(System.in);
+       //ask for name
+        System.out.println("What is your name?: ");
+        String name= input.nextLine();
+       // Ask for what test was out of and what percentage they got
+        System.out.println("What was the first test out of?: ");
+        double test1 = input.nextDouble();
+        System.out.println("What mark did you get?:");
+        double test1results =input.nextDouble();
+        System.out.println("What was the second test out of?: ");
+        double test2 = input.nextDouble();
+        System.out.println("What mark did you get?: ");
+        double test2results = input.nextDouble();
+        System.out.println("What was the third test out of?: ");
+        double test3 = input.nextDouble();
+        System.out.println("What mark did you get?:");
+        double test3results =input.nextDouble();
+        System.out.println("What was the fourth test out of?: ");
+        double test4 = input.nextDouble();
+        System.out.println("What mark did you get?:");
+        double test4results =input.nextDouble();
+        System.out.println("What was the fifth test out of?: ");
+        double test5 = input.nextDouble();
+        System.out.println("What mark did you get?:");
+        double test5results =input.nextDouble();
+        double 
+        
+        
        
        
        
