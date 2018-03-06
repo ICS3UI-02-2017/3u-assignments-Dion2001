9c0488e35ed3f3a52f96af849cde096cdb894149
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-03-06 11:18:48 -0500

    A4Q5

---
+++ src/Assignment4/A4Q6.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author salod8556
+ */
+public class A4Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create scanner 
+        Scanner input = new Scanner(System.in);
+        
+        //create fines per speed limits
+        System.out.println("Enter the speed limit:  ");
+        int Limit= input.nextInt();
+        System.out.println("Enter the recorded speed of the car: ");
+        int speed= input.nextInt();
+        
+        //message for the the speed you are going\
+        int 
+    }
+}
