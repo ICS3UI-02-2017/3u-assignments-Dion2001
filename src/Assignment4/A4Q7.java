9f0c336236da0d17f429ff8b2d6a4394ba849a3f
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-03-08 11:15:12 -0500

    A4Q7 Finished

--- src/Assignment4/A4Q7.java
+++ src/Assignment4/A4Q7.java
@@ -17,11 +17,54 @@ public class A4Q7 {
      */
     public static void main(String[] args) {
         // create scanner
-      Scanner input= new Scanner(System.in);
-      //create dice
-       System.out.println("Enter sum of dice: ");
-      int numberOfMoves= input.nextInt();
-        System.out.println("You are now on square: " + numberOfMoves);
-      int placement= 1;
+        Scanner input = new Scanner(System.in);
+        //create dice
+        int finalsquare = 1;
+        while (finalsquare != 100) {
+            System.out.println("Enter sum of dice: ");
+            int numberOfMoves = input.nextInt();
+
+            if (numberOfMoves < 2 || numberOfMoves > 12) {
+                System.out.println("You Quit!");
+                System.exit(numberOfMoves);
+            } else {
+                finalsquare = numberOfMoves + finalsquare;
+
+
+                if (finalsquare == 54) {
+                    finalsquare = 19;
+                }
+                if (finalsquare == 90) {
+                    finalsquare = 48;
+                }
+                if (finalsquare == 99) {
+                    finalsquare = 78;
+                }
+                if (finalsquare == 9) {
+                    finalsquare = 34;
+                }
+                if (finalsquare == 40) {
+                    finalsquare = 64;
+                }
+                if (finalsquare == 67) {
+                    finalsquare = 86;
+                }
+
+                if (finalsquare > 100) {
+                    finalsquare = finalsquare - numberOfMoves;
+                }
+
+                System.out.println("You are now on square: " + finalsquare);
+                if (finalsquare == 100) {
+                    System.out.println("You Win!");
+                }
+
+            }
+
+
+
+
+
+        }
     }
 }
