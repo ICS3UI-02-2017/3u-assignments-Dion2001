90d35236a38eafe287174f56e2f5ca30506b54ed
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-03-22 11:01:05 -0400

    A4Q10 Finished

---
+++ src/Assignment5/A5Q1.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author salod8556
+ */
+public class A5Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Make sure the program doesn't stop
+
+        while (true) {
+
+            // ask for word to translate
+            System.out.println("Enter Word to Translate: ");
+            String oldWord = input.nextLine();
+
+            // Sanitize word to lowercase
+            oldWord = oldWord.toLowerCase();
+            
+            //look for vowels in word
+             int length = oldWord.length();
+
+            // Reconize all vowels
+             for (int i = 0; i < length; i++) {
+            if (oldWord.charAt(i) == 'a' || oldWord.charAt(i) == 'e' || oldWord.charAt(i) == 'i' || oldWord.charAt(i) == 'o' || oldWord.charAt(i) == 'u') {
+                
+            // place "ub" after the first vowel
+                String firstV = oldWord.substring(0, i);
+                String endW = oldWord.substring(i, 0);
+                System.out.println(firstV+ "ub" + endW);
+            }
+        }
+    }
+}
+}
