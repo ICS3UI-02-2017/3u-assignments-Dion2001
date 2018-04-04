374a39a608ca6bc353f70c08b2675680dbf37c9c
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-04-04 11:17:46 -0400

    A6Q3  Progress

---
+++ src/Assignment6/A6Q3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author salod8556
+ */
+public class A6Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner input= new Scanner(System.in);
+       
+       // Enter two numbers 
+        System.out.println("Enter 2 integers");
+        int numbers =input.nextLine();
+       
+       
+       int[] num= new int[numbers];
+       
+       
+       
+       
+    }
+}
