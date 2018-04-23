ec90d966224d5ac3e29ad52cc6a90dba37c4e644
salod8556 <salod8556@HRH-IC0027326.wrdsb.ca>
2018-04-23 11:10:52 -0400

    A6Q7 Done

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -18,45 +18,29 @@ public class A6Q7 {
     public static void main(String[] args) {
 
         // create a array  with numbers from 2 to 1000
-        int[] numbers = new int[999];
-        int p = 2;
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = p;
-            p = p + 1;
+        boolean prime[] = new boolean[999];
+        for (int i = 0; i < 998; i++) {
+            prime[i] = true;
+        }
 
-
-            if (numbers[i] % 2 == 0) {
-                numbers[i] = 0;
-                numbers[0] = 2;
-            }
-            if (numbers[i] % 3 == 0) {
-                numbers[i] = 0;
-                numbers[1] = 3;
-            }
-            if (numbers[i] % 5 == 0) {
-                numbers[i] = 0;
-                numbers[3] = 5;
-            }
-            if (numbers[i] % 7 == 0) {
-                numbers[i] = 0;
-                numbers[5] = 7;
-            }
-            if (numbers[i] % 9 == 0) {
-                numbers[i] = 0;
-                numbers[7] = 9;
-            }
-            if(numbers[i] % 11 ==0){
-                numbers[i]= 0;
-                numbers [9] = 11;                
-            }
-            if(numbers[i] %13 ==0){
-                 numbers[i] = 0;
-                 numbers [11] =13;
+        for(int n =2; n *n <=998; n++ ) {
+            if (prime[n] == true){
+            for(int i =n * 2; i <999; i+=n){
+                prime[i]= false;
+                
+           
+            
+        }
+            
+        }
+    }
+        for(int i=2; i <999; i++){
+            if (prime[i] ==true) {
+                System.out.print(i +", ");
             }
         }
-        
-
-
-        System.out.println(Arrays.toString(numbers));
-    }
+            
 }
+    
+}
+
