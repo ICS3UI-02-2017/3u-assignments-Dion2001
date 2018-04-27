190f4da000bf8c18bf8463e9c5c687a77309433e
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-04-27 11:19:10 -0400

    A7Q6 Progress

--- src/Assignment7/A7Q6.java
+++ src/Assignment7/A7Q6.java
@@ -12,40 +12,48 @@ import java.util.Scanner;
  */
 public class A7Q6 {
 
-    public static void lastDigit(double number) {
-        if (number % 10 == 0) {
-            System.out.println("The last digit of your number is 0");
-        } else if (number % 10 == 1) {
-            System.out.println("The last digit of your number is 1");
-        } else if (number % 10 == 2) {
-            System.out.println("The last digit of your number is 2");
-        } else if (number % 10 == 3) {
-            System.out.println("The last digit of your number is 3");
-        } else if (number % 10 == 4) {
-            System.out.println("The last digit of your number is 4");
-        } else if (number % 10 == 5) {
-            System.out.println("The last digit of your number is 5");
-        } else if (number % 10 == 6) {
-            System.out.println("The last digit of your number is 6");
-        } else if (number % 10 == 7) {
-            System.out.println("The last digit of your number is 7");
-        } else if (number % 10 == 8) {
-            System.out.println("The last digit of your number is 8");
-        } else if (number % 10 == 9) {
-            System.out.println("The last digit of your number is 9");
-
+    public static int lastDigit(int number) {
+        int b=0;
+        for (int i = 0; i < 10; i++) {
+             if (number % 10 == i) {
+           b = i;
+          return b;
         }
+        
+        }
+    }
+//        } else if (number % 10 == 1) {
+//            System.out.println("The last digit of your number is 1");
+//        } else if (number % 10 == 2) {
+//            System.out.println("The last digit of your number is 2");
+//        } else if (number % 10 == 3) {
+//            System.out.println("The last digit of your number is 3");
+//        } else if (number % 10 == 4) {
+//            System.out.println("The last digit of your number is 4");
+//        } else if (number % 10 == 5) {
+//            System.out.println("The last digit of your number is 5");
+//        } else if (number % 10 == 6) {
+//            System.out.println("The last digit of your number is 6");
+//        } else if (number % 10 == 7) {
+//            System.out.println("The last digit of your number is 7");
+//        } else if (number % 10 == 8) {
+//            System.out.println("The last digit of your number is 8");
+//        } else if (number % 10 == 9) {
+//            System.out.println("The last digit of your number is 9");
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What number would you like to find the last number of?");
+        
+        int digit =lastDigit(input.nextInt());
 
     }
+        }
+
+    
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("What number would you like to find the last number of?");
-        lastDigit(input.nextDouble());
-        
-
-    }
+    
 }
