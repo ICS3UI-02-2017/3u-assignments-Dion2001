fd34106a62313b4e95fffc994ae38f68cba04040
salod8556 <salod8556@HRH-IC0027326.wrdsb.ca>
2018-04-30 11:19:32 -0400

    A7Q7 Done

--- src/Assignment7/A7Q7.java
+++ src/Assignment7/A7Q7.java
@@ -4,19 +4,32 @@
  */
 package Assignment7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author salod8556
  */
 public class A7Q7 {
 
-    public static void allDigitsOdd(boolean a){
-        
+    public static int firstDigit(int firstNum){
+        int first =0;
+        for (int i = 1; i < 10; i++) {
+            if(firstNum % i==i){
+                first = i;
+            }
+        }
+        return first;
     }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("What number would you like to find the first number of?");
+        // out put the number of the remainder
+        int digit = firstDigit(input.nextInt());
+        System.out.println("The first digit of your number is: " + digit);
     }
-}
+    }
+
