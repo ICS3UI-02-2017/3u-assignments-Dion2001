1b86524852a49f51e92aac25765550eefe9eee7a
salod8556 <salod8556@HRH-IC0027328.wrdsb.ca>
2018-05-11 11:16:25 -0400

    Smile Face Animation Done

--- src/Assignment8/SmileyFace.java
+++ src/Assignment8/SmileyFace.java
@@ -24,41 +24,38 @@ public class SmileyFace extends JComponent implements ActionListener {
     // Height and Width of our game
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
-
     //Title of the window
     String title = "My Game";
-
     // sets the framerate and delay for our game
     // this calculates the number of milliseconds per frame
     // you just need to select an approproate framerate
     int desiredFPS = 60;
     int desiredTime = Math.round((1000 / desiredFPS));
-    
     // timer used to run the game loop
     // this is what keeps our time running smoothly :)
     Timer gameTimer;
-
     // YOUR GAME VARIABLES WOULD GO HERE
     Color skin = new Color(237, 213, 37);
-    Color mouth = new Color (137, 24, 78);
-    Color tounge = new Color (239, 45, 116);
-    Color eyebrow = new Color (119, 80, 8);
-    
+    Color mouth = new Color(137, 24, 78);
+    Color tounge = new Color(239, 45, 116);
+    Color eyebrow = new Color(119, 80, 8);
+    // Variables for left Eye
     int leftEyeX = 250;
     int leftEyeMovement = -1;
-    int rightEyex= 492;
+    // Variables for Right Eye
+    int rightEyex = 492;
     int rightEyeMovement = -1;
-    int rightEyeBrowx= 159;
+    // Variables for right Eye Brow
+    int rightEyeBrowx = 159;
     int righteyeBrowMovement = -1;
-    
-
+    // Variables for left Eye Brow
+    int leftEyeBrowx = 159;
+    int lefteyeBrowMovement = -1;
 
     // GAME VARIABLES END HERE    
-
-    
     // Constructor to create the Frame and place the panel in
     // You will learn more about this in Grade 12 :)
-    public SmileyFace(){
+    public SmileyFace() {
         // creates a windows to show my game
         JFrame frame = new JFrame(title);
 
@@ -80,8 +77,8 @@ public class SmileyFace extends JComponent implements ActionListener {
         this.addMouseMotionListener(m);
         this.addMouseWheelListener(m);
         this.addMouseListener(m);
-        
-        gameTimer = new Timer(desiredTime,this);
+
+        gameTimer = new Timer(desiredTime, this);
         gameTimer.setRepeats(true);
         gameTimer.start();
     }
@@ -91,8 +88,8 @@ public class SmileyFace extends JComponent implements ActionListener {
     // NOTE: This is already double buffered!(helps with framerate/speed)
     @Override
     public void paintComponent(Graphics g) {
-        Graphics2D g2d = (Graphics2D)g;
-        
+        Graphics2D g2d = (Graphics2D) g;
+
         // always clear the screen first!
         g.clearRect(0, 0, WIDTH, HEIGHT);
 
@@ -119,13 +116,13 @@ public class SmileyFace extends JComponent implements ActionListener {
         // Rotate the mouth filler
         g.translate(371, 509);
         g2d.rotate(Math.toRadians(-26));
-        g.fillArc(0, -12, 175, 50, 180, 180 );
+        g.fillArc(0, -12, 175, 50, 180, 180);
         g2d.rotate(Math.toRadians(26));
         g.translate(-371, -509);
         // create eyebrows
         g.setColor(eyebrow);
         g.fillRoundRect(183, rightEyeBrowx, 125, 30, 25, 25);
-        g.fillRoundRect(430, 159, 125, 30, 25, 25);
+        g.fillRoundRect(430, leftEyeBrowx, 125, 30, 25, 25);
         // GAME DRAWING ENDS HERE
     }
 
@@ -133,64 +130,65 @@ public class SmileyFace extends JComponent implements ActionListener {
     // This is run before the game loop begins!
     public void preSetup() {
         // Any of your pre setup before the loop starts should go here
-
     }
 
     // The main game loop
     // In here is where all the logic for my game will go
     public void gameLoop() {
         leftEyeX = leftEyeX + leftEyeMovement;
-       
-              
-        
-        if(leftEyeX < 200){
+        if (leftEyeX < 200) {
             leftEyeMovement = 1;
         }
-        if(leftEyeX > 250){   
+        if (leftEyeX > 250) {
             leftEyeMovement = -1;
-            
-}  
-        rightEyex = rightEyex + rightEyeMovement;
-        if(rightEyex < 442){
-            rightEyeMovement = 1; 
         }
-        if(rightEyex > 492){
+        rightEyex = rightEyex + rightEyeMovement;
+        if (rightEyex < 442) {
+            rightEyeMovement = 1;
+        }
+        if (rightEyex > 492) {
             rightEyeMovement = -1;
         }
-        rightEyeBrowx = rightEyeBrowx +  righteyeBrowMovement;
-        if(rightEyeBrowx < 100){
+        rightEyeBrowx = rightEyeBrowx + righteyeBrowMovement;
+        if (rightEyeBrowx < 109) {
             righteyeBrowMovement = 1;
         }
-        
-}
+        if (rightEyeBrowx > 159) {
+            righteyeBrowMovement = -1;
+        }
+        if (leftEyeBrowx < 109) {
+            lefteyeBrowMovement = 1;
+        }
+        leftEyeBrowx = leftEyeBrowx + lefteyeBrowMovement;
+        if (leftEyeBrowx < 109) {
+            lefteyeBrowMovement = 1;
+        }
+        if (leftEyeBrowx > 159) {
+            lefteyeBrowMovement = -1;
+        }
+    }
 
-
-    
     // Used to implement any of the Mouse Actions
     private class Mouse extends MouseAdapter {
 
         // if a mouse button has been pressed down
         @Override
         public void mousePressed(MouseEvent e) {
-
         }
 
         // if a mouse button has been released
         @Override
         public void mouseReleased(MouseEvent e) {
-
         }
 
         // if the scroll wheel has been moved
         @Override
         public void mouseWheelMoved(MouseWheelEvent e) {
-
         }
 
         // if the mouse has moved positions
         @Override
         public void mouseMoved(MouseEvent e) {
-
         }
     }
 
@@ -200,13 +198,11 @@ public class SmileyFace extends JComponent implements ActionListener {
         // if a key has been pressed down
         @Override
         public void keyPressed(KeyEvent e) {
-
         }
 
         // if a key has been released
         @Override
         public void keyReleased(KeyEvent e) {
-
         }
     }
 
@@ -225,4 +221,3 @@ public class SmileyFace extends JComponent implements ActionListener {
         SmileyFace game = new SmileyFace();
     }
 }
-
